0c9c4fb88368686e6cfc927e28959719a05a6726
Stenio L. De Souza <steniols@gmail.com>
2023-06-29 00:09:07 -0300

    Add site crud

---
+++ app/Http/Controllers/Admin/SiteController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSiteRequest;
+use App\Models\Site;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $sites = Site::all();
+
+        return view('admin/sites/index', compact('sites'));
+    }
+
+    public function create()
+    {
+        return view('admin/sites/create');
+    }
+
+    public function store(StoreUpdateSiteRequest $request)
+    {
+        //$user = Auth::user();
+        $user = auth()->user();
+        $user->sites()->create($request->all());
+
+        return redirect()
+                ->route('sites.index')
+                ->with('message', 'Site criado com sucesso');
+    }
+
+    public function edit(string $id)
+    {
+        if (!$site = Site::find($id)) {
+            return back();
+        }
+
+        return view('admin/sites/edit', compact('site'));
+    }
+
+    public function update(StoreUpdateSiteRequest $request, Site $site)
+    {
+        $site->update($request->validated());
+
+        return redirect()
+                ->route('sites.index')
+                ->with('message', 'Site atualizado com sucesso');
+    }
+
+    public function destroy(Site $site)
+    {
+        $site->delete($site);
+
+        return redirect()
+                ->route('sites.index')
+                ->with('message', 'Site excluído com sucesso');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\Admin\SiteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('sites', [SiteController::class, 'index'])->name('sites.index');
+    Route::get('sites/create', [SiteController::class, 'create'])->name('sites.create');
+    Route::get('sites/{site}/edit/', [SiteController::class, 'edit'])->name('sites.edit');
+    Route::post('sites', [SiteController::class, 'store'])->name('sites.store');
+    Route::put('sites/{site}', [SiteController::class, 'update'])->name('sites.update');
+    Route::delete('sites/{site}', [SiteController::class, 'destroy'])->name('sites.destroy');
+
+});
+
+require __DIR__.'/auth.php';
